5cd8183c08c5e59cb1f892d640379f42cda69def
Chrro1999 <christian.bolanos@uisek.edu.ec>
2025-11-11 10:34:12 -0500

    Examenen acabado

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
+# movies/models.py
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=100)
+    director = models.CharField(max_length=150)
+    release_year = models.PositiveIntegerField()  
+    synopsis = models.TextField()
+
+    def __str__(self):
+        return f"{self.title} ({self.release_year})"
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Movie
 
-# Create your views here.
+def movie_list(request):
+    movies = Movie.objects.all()
+    return render(request, 'movies/movie_list.html', {'movies': movies})
+def movie_detail(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    return render(request, 'movies/movie_detail.html', {'movie': movie})
+
+
+
