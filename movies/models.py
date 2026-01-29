39a4701957669b3e94780206edfa36d40edd7bc8
Chrro1999 <christian.bolanos@uisek.edu.ec>
2026-01-29 09:24:22 -0500

    Merge branch 'main' of https://github.com/Chrro1999/ExamenFinal

--- movies/models.py
+++ movies/models.py
@@ -18,6 +18,8 @@ class Meta:
 def __str__(self):
         return self.name
 
+#Modelos para la aplicacion de peliculas
+
 class Movie(models.Model):
     title = models.CharField(max_length=200)
     genre = models.CharField(max_length=100)
--- movies/views.py
+++ movies/views.py
@@ -1,14 +1,38 @@
 from rest_frmaework import viewsets, filters, status
-from rest_framewor
-from django.shortcuts import render
-from .models import Movie, Director
+from rest_framework import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
-def movie_list(request):
-    movies = Movie.objects.all()
-    return render(request, 'movies/movie_list.html', {'movies': movies})
-def movie_detail(request, pk):
-    movie = Movie.objects.get(pk=pk)
-    return render(request, 'movies/movie_detail.html', {'movie': movie})
+from .models import Movie, Director
+from .serializers import (
+    Movieserializer,
+    DirectorSerializer,
+    DirectorDetailSerializer
+)
+
+class DirectorViewSet(viewsets.ModelViewSet):
+    queryset = Director.objects.all()
+    serializer_class = DirectorSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'nationality']
+    ordering_fields = ['name', 'created_at']
+    ordering = ['name']
+
+def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return DirectorDetailSerializer
+        return DirectorSerializer
+
+@action(detail=True, methods=['get'])
+def movies(self, request, pk=None):
+    director = self.get_object()
+    movies = director.movies.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
+
+
 
 
 
